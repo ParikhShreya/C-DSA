173dfcbd627f093485732b161c4262adc74486ca
ParikhShreya <shreyaparikh2006@gmail.com>
2025-06-01 19:54:44 +0530

    Add files via upload

--- Patterns.cpp
+++ Patterns.cpp
@@ -14,9 +14,9 @@ using namespace std;
 *****
 *****
 */
-void pattern1(int rows, int colm) {
+void pattern1(int rows) {
     for (int i = 1; i <= rows; i++) {
-        for (int j = 1; j <= colm; j++) {
+        for (int j = 1; j <= rows; j++) {
             cout << "*";
         }
         cout << endl;
@@ -46,7 +46,6 @@ void pattern2(int rows) {
 1 2 3 4 
 1 2 3  4 5
 */
-
 
 void pattern3(int rows) {
     for (int i = 1; i <= rows; i++) {
@@ -82,7 +81,6 @@ void pattern4(int rows) {
 *
 */
 
-
 void pattern5(int rows){
 for(int i =1; i<=rows;i++){
 for(int j = 1; j<=rows-i+1;j++){
@@ -91,7 +89,7 @@ cout<<"*";
 cout<<endl;
 }
 }
-              
+           
 /*PATTERN 6
 1 2 3 4 5
 1 2 3 4
@@ -99,7 +97,6 @@ cout<<endl;
 1 2 
 1
 */
-
 void pattern6(int rows) {
     for (int i =1; i <=rows; i++) {
         for (int j = 1; j<=rows-i+1; j++) {
@@ -108,8 +105,6 @@ void pattern6(int rows) {
         cout << endl;
     }
 }
-
-
 /*PATTERN 7
 
         *
@@ -193,8 +188,6 @@ THIS PATTERN IS COMBINATION OF THE PATTERN 7,8 */
 * *
 *
 */
-
-
    void pattern10(int rows){
 // Upper triangle
     for (int i = 1; i <= rows; i++) {
@@ -213,10 +206,6 @@ THIS PATTERN IS COMBINATION OF THE PATTERN 7,8 */
     }
    }
     
-
-
-
-
 /*PATTERN 11
 1
 0 1
@@ -235,66 +224,285 @@ void pattern11(int rows) {
         cout << endl;
     }
 }
+/*PATTERN 12
+1             1
+1 2         2 1
+1 2 3     3 2 1
+1 2 3 4 4 3 2 1           
+*/
+void pattern12(int rows) {
 
+    for (int i = 1; i <= rows; i++) {
+        // Increasing numbers
+        for (int num = 1; num <= i; num++) {
+            cout << num << " ";
+        }
 
-int main() {
-    int rows, colm;
+        // Middle spaces
+        int middleSpaces = 2 * (rows - i);
+        for (int s = 1; s <= middleSpaces; s++) {
+            cout << "  ";  // Two spaces for better spacing
+        }
 
-    // Input for Pattern 1
-    cout << "Enter number of rows and columns for Pattern 1: ";
-    cin >> rows >> colm;
-    pattern1(rows, colm);
+        // Decreasing numbers
+        for (int num = i; num >= 1; num--) {
+            cout << num << " ";
+        }
 
-    // Input for Pattern 2
-    cout << "\nEnter number of rows for Pattern 2: ";
-    cin >> rows;
-    pattern2(rows);
+        cout << endl;
+    }  
+}
 
-    //Input for Pattern 3
-    cout << "\nEnter number of rows for Pattern 3: ";
-    cin >> rows;
-    pattern3(rows);
+/*PATTERN 13
+1
+2 3
+4 5 6
+7 8 9 10
+*/
 
-    //Input for Pattern 4
-    cout << "\nEnter number of rows for Pattern 4: ";
-    cin >> rows;
-    pattern4(rows);
+void pattern13(int n){
+    int num =1;
+    for(int i =1;i<=n;i++){
+        for(int j =1;j<=i;j++){
+          cout<<num;
+          num = num +1;
+        }cout<<endl;
+    }
+}
 
-    //Input for Pattern 5
-    cout << "\nEnter number of rows for Pattern 5: ";
-    cin >> rows;
-    pattern5(rows);
+/*PATTERN 14
+A
+A B
+A B C
+A B C D
+*/
 
+void pattern14(int rows){
     
-    //Input for Pattern 6
-    cout << "\nEnter number of rows for Pattern 6: ";
-    cin >> rows;
-    pattern6(rows);
+    for(int i = 1; i<=rows; i++){  
+       for(char ch = 'A'; ch<'A'+i;ch++){
+        cout<<ch<<" ";
+       }cout<<endl;
+    }
+}
+/*PATTERN 15
+A A A A A
+B B B B
+C C C
+D D
+E
+*/
+void pattern15(int rows){
+    
+    for(int i = rows;i>=1;i--){
+        char ch = 'A' +(rows-i);
+        for(int j =1;j<=i;j++){
+            cout<<ch<<" ";
+        }cout <<endl;
+    }
+}
 
-    //Input for Pattern 7
-    cout << "\nEnter number of rows for Pattern 7: ";
-    cin >> rows;
-    pattern7(rows);
+/*PATTERN 16
+A
+B B
+C C C
+D D D D
+E E E E E
+*/
 
-    //Input for Pattern 8
-    cout << "\nEnter number of rows for Pattern 8: ";
-    cin >> rows;
-    pattern8(rows);
+void pattern16(int rows){
+    
+    for(int i =0;i<rows;i++){
+        char ch ='A'+i;
+        for(int j = 0; j<=i;j++){
+           cout<<ch<<" ";
+           
+        }cout<<endl;
+    }
+}
+/*PATTERN 17
+A B C D E
+A B C D
+A B C
+A B
+A
+*/
+void pattern17(int rows){
+    for(int i = 0;i<rows;i++){
+        for(char ch = 'A'; ch<='A'+(rows-i-1);ch++){
+            cout<<ch<<" ";    
+        }cout<<endl;
+    }
+}
 
-    //Input for Pattern 9
-    cout << "\nEnter number of rows for Pattern 9: ";
-    cin >> rows;
-    pattern7(rows);
-   pattern8(rows);
+/*PATTERN 18
+                A
+              A B A
+            A B C B A
+          A B C D C B A  */
 
-    //Input for Pattern 10
-    cout << "\nEnter number of rows for Pattern 10: ";
-    cin >> rows;
-    pattern10(rows);
+    void pattern18(int rows) {
+    //NUMBER OF LINES
+    for (int i =0; i <rows; i++) {
+    
+        //SPACE 
+        for(int space = 0; space<rows-i-1;space++) {
+            cout<<" ";
+        }
+        // CHARACTER
+        char ch = 'A';
+        int breakpoint = (2*i+1) / 2;
+         for(int j = 1; j<=2*i+1;j++){
+            cout<<ch<<" ";
+            if(j<=breakpoint) {
+                ch++;
+            }else ch--;
+            
+         }
+        //SPACE
+        for(int space = 0; space<rows-i-1;space++) {
+            cout<<" ";
+        }
+        cout<<endl;
 
-    //Input for Pattern 11
-    cout << "\nEnter number of rows for Pattern 11: ";
+    }
+}
+
+/*PATTRN 19
+E
+D E
+C D E
+B C D E
+A B C D E
+*/
+
+void pattern19(int rows){
+    for(int i = 0;i<rows;i++){
+        for(char ch = 'E'-i;ch<='E';ch++){
+            cout<<ch<<" ";
+        } cout<<endl;
+    }
+}
+
+/*PATTERN 20
+
+* * * * * * * * * *
+* * * *     * * * *
+* * *         * * *
+* *             * *
+*                 *
+*                 *
+* *             * *
+* * *         * * *
+* * * *     * * * *
+* * * * * * * * * *
+*/
+void pattern20(int rows) {
+    // Upper half
+    for (int i = 0; i < rows; i++) {
+        // Left stars
+        for (int star = 0; star < rows - i; star++) {
+            cout << "*";
+        }
+        // Spaces in the middle
+        for (int sp = 0; sp < 2 * i; sp++) {
+            cout << " ";
+        }
+        // Right stars
+        for (int star = 0; star < rows - i; star++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Lower half
+    for (int i = 0; i < rows; i++) {
+        // Left stars
+        for (int star = 0; star <= i; star++) {
+            cout << "*";
+        }
+        // Spaces in the middle
+        for (int sp = 0; sp < 2 * (rows - i - 1); sp++) {
+            cout << " ";
+        }
+        // Right stars
+        for (int star = 0; star <= i; star++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+/*PATTERN 21
+*                 *
+* *             * *
+* * *         * * *
+* * * *     * * * *
+* * * * * * * * * *
+* * * *     * * * *
+* * *         * * *
+* *             * *
+*                 *
+*/
+
+void pattern21(int rows){
+    for(int i = 0;i<rows;i++){
+          //star
+          for(int star = 0;star<=i;star++){
+            cout<<"*";
+          }
+          //space
+          for(int space =0; space< 2*(rows-i-1);space++){
+            cout<<" ";
+          }
+          //star
+          for(int star = 0;star<=i;star++){
+            cout<<"*";
+          }cout<<endl;
+    }
+
+    //lower half
+    for(int i =1;i<rows;i++){
+        //star
+        for(int star = 0; star<rows-i;star++){
+            cout<<"*";
+        }
+        //space
+        for(int space =0; space<2*i;space++){
+            cout<<" ";
+        }
+        //star
+        for(int star= 0;star<rows-i;star++){
+            cout<<"*";
+        }cout<<endl;
+    }
+}
+
+/*PATTERN  22
+* * * * 
+*     *
+*     *
+* * * *
+*/
+
+void pattern22(int rows){
+    for(int i =0;i<rows;i++){
+        for(int j = 0; j<rows; j++){
+          if(i==0||j==0||i==rows-1||j==rows-1){
+            cout<<"*";
+          } else{
+            cout<<" ";
+          } 
+        }cout<<endl;
+    }
+}
+int main() {
+    int rows;
+
+    // Input for Pattern 
+    cout << "Enter number of rows : ";
     cin >> rows;
-    pattern11(rows);
+    pattern22(rows);
+
     return 0;
 }
